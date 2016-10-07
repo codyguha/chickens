79c46a3e2ff9f32c84437d0785bb4b465348e1fe
codyguha <aalo.guha@gmail.com>
2016-10-07 13:23:23 -0700

    heroku

--- config/index.js
+++ config/index.js
@@ -4,7 +4,7 @@ module.exports = {
     kik: {
       apiKey: 'ea2ff779-c0cb-4ee8-9e22-377629a80c5d', // [Non functional example] 0a275f34-31da-4ds1-b3c9-d4576f38bac8
       botUsername: 'surveychicken', // [Non functional example] startkitbot
-      baseUrl: 'http://b5fecdeb.ngrok.io' // [Non functional example] http://4b7e3c1e.ngrok.io
+      baseUrl: 'https://glacial-reef-68882.herokuapp.com' // [Non functional example] http://4b7e3c1e.ngrok.io
     }
   }
 }['local']
