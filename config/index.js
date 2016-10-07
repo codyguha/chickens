4b3a0ccfdcf987f548922f00a865376141238e54
codyguha <aalo.guha@gmail.com>
2016-10-07 13:14:09 -0700

    first commit

---
+++ config/index.js
@@ -0,0 +1,10 @@
+module.exports = {
+  local: {
+    port: 8080,
+    kik: {
+      apiKey: 'ea2ff779-c0cb-4ee8-9e22-377629a80c5d', // [Non functional example] 0a275f34-31da-4ds1-b3c9-d4576f38bac8
+      botUsername: 'surveychicken', // [Non functional example] startkitbot
+      baseUrl: 'http://b5fecdeb.ngrok.io' // [Non functional example] http://4b7e3c1e.ngrok.io
+    }
+  }
+}['local']
